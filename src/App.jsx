c4d6fd03531e388b74d720b7c47cab05930ea341
Dipti Jain <deeptihnd000@gmail.com>
2024-06-25 00:46:23 +0530

    installed tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
-
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_PROJECT_ID);
-  return (
-    <>
-      <div>Hello </div>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return !loading ? <div className="min-h-screen"></div> : null;
 }
 
 export default App;
