5ad9a63764ce98799e506b8ec6f2857833955880
Dipti Jain <deeptihnd000@gmail.com>
2024-06-26 13:33:50 +0530

    created header

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,7 +1,7 @@
 import React, { Children } from "react";
 
 function Container() {
-  return <div className="w-full max-w-7xl mx-auto px-4">{children}</div>;
+  return <div className="w-full max-w-7xl mx-auto px-4">{Children}</div>;
 }
 
 export default Container;
