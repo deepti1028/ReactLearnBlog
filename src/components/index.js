7cabd91875f03b6c500291fd033bca42e9d254d1
Dipti Jain <deeptihnd000@gmail.com>
2024-06-28 00:10:01 +0530

    created post form component

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutButton from "./Header/LogoutButton";
-export { Header, Footer, LogoutButton, Logo, Container };
+import RTE from "./RTE";
+export { Header, Footer, LogoutButton, Logo, Container, RTE };
